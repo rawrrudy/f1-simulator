c244c67e7dc06772efce7ddc66322d5d15c6fb38
Rudra Sundgikar <rudrasundgikar@gmail.com>
2026-07-27 15:31:02 +0530

    Made changes to driver selection page

---
+++ src/game/screens/DriverSelection/DriverSelection.tsx
@@ -0,0 +1,45 @@
+import "./DriverSelection.css";
+
+export function DriverSelection() {
+  return (
+    <div className="driver-selection">
+      
+      <div className="driver-background" />
+
+      <div className="team-header">
+
+        <img
+          className="team-logo"
+          src=""
+          alt=""
+        />
+
+        <h1>SCUDERIA FERRARI</h1>
+
+      </div>
+
+      <div className="driver-layout">
+
+        <div className="driver-left">
+
+            LEFT DRIVER
+
+        </div>
+
+        <div className="driver-info">
+
+            INFO CARD 
+
+        </div>
+
+        <div className="driver-right">
+
+            RIGHT DRIVER
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
